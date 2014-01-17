98c665324495fe66bdf4f2e22330370d469e2a2f
cgarciaarano <cgarciaarano@gmail.com>
2014-01-17 18:38:32 +0100

    First classes

---
+++ core/action.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+action.py
+
+Action
+
+2014 Pogona 
+
+@author Carlos Garcia <cgarciaarano@gmail.com>
+"""
+import logging
+from event_queue import EventQueue
+from datetime import datetime
+
+logger = logging.getLogger('web')
+
+class Action(object):
+	''''
+	Represents the action taken for a given event. It's an abstract class and should not be instantiated directly.
+	Provides the attribs and public methods execute() and callback()
+	'''
+
+	def __init__(self, *event_dict, **kwargs):
+		for key in event_dict:
+			setattr(self, key, event_dict[key])
+		for key in kwargs:
+			setattr(self, key, kwargs[key])
+
+	def __init__(self, event):
+		self.event = event
+		self.action_type = self.__class__.__name__
+
+	def __str__(self):
+		return str(self.__dict__)
+
+	def get_type(self):
+		return self.action_type
+
+	@staticmethod
+	def subclass():
+		''' Returns a dict with subclasses names and subclasses instances.
+		P.e. {'SimpleCall': SimpleCall, 'AcknowledgedCall': AcknowledgedCall}.
+		Quite useful when adding Actions ;) '''
+		return dict((subclass.__name__, subclass) for subclass in Action.__subclasses__())
+
+	def get_data(self):
+		return str(self)		
+
+	def execute(self):
+		pass
+
+	def callback(self,result):
+		if result:
+			# success
+			pass
+		else:
+			# Increment event step
+			self.event.incr_step()
+			
+			# Push back on EventQueue
+			q = EventQueue()
+			q.push_event(self.event)
+
+
+
+class SimpleCall(Action):
+	"""
+	Represents a simple call through Asterisk. 
+	Place a call to 'ddi' from 'cli' for 'duration' seconds, and retries 'retries' times
+	Inherits from Action, so execute() and callback() are implemented.
+	"""
+	def __init__(self, event, params=None):
+		Action.__init__(self, event)
+		self.action_type = self.__class__.__name__
+		self.params = {	ddi: None,
+						cli: None,
+						retries: 1,
+						duration:2,
+					}
+
+	def execute(self):
+		# Some blocking execution
+		result = True 
+
+		self.callback(result)
+
+
+class AcknoweledgedCall(Action):
+	"""
+	Represents an acknowledged call through Asterisk. 
+	Place a call to 'ddi' from 'cli' until the called party sends back 'pin' through DTMF, and retries 'retries' times
+	Inherits from Action, so execute() and callback() are implemented.
+	"""
+	self.action_type = self.__class__.__name__
+	
+	def __init__(self, event, params=None):
+		Action.__init__(self, event)
+		self.action_type = self.__class__.__name__
+		self.params = {	ddi: None,
+						cli: None,
+						pin: None,
+					}
+
+	def execute(self):
+		# Some blocking execution
+		result = True 
+
+		self.callback(result)
---
+++ core/event_queue.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+event_queue.py
+
+Event Queue
+
+2014 Pogona 
+
+@author Carlos Garcia <cgarciaarano@gmail.com>
+"""
+from event import Event
+
+class EventQueue():
+	"""
+	Queue of events. It uses Redis as backend, and permits duplicated events.
+	Pop is blocked until a new event is pushed.
+	"""
+	def __init__(self):
+		self.queue = 'EVENT_QUEUE'
+		self.redis = self.redis_connect()
+
+	def redis_connect():
+		''' Reconnects to Redis and returns an active connection. Exits the program if it can't '''
+		attempts = 1
+		got_redis = False
+		
+		while not got_redis and attempts < 10:
+			try:
+				logger.info("Trying Redis reconnection {0}...".format(attempts))
+				rds = redis.Redis(options.redisIP,db=options.redisDB)
+				rds.llen(self.queue)
+				got_redis = True
+				logger.info("Redis connected!")
+			except Exception, e:
+				logger.error("Could not connect to Redis at {0}: {1}. Trying again in 1 second".format(options.redisIP,e))
+				time.sleep(1)
+				attempts += 1
+
+		if attempts == 10:
+			logger.error("Reconnection to Redis {0} failed 10 times, exiting program".format(options.redisIP))
+			sys.exit(-1)
+
+		return rds
+
+
+	def pop_event(self):
+		try:
+			event = self.redis.blpop(self.queue)[1]
+
+			event = eval(event)
+			total = self.backend.incr(CONSUMED_EVENTS,1) # Increment in redis the number of events consumed 
+			logger.debug("Total events consumed {0}, processing...".format(total))
+		except redis.ConnectionError:
+			pass
+
+		return Event(event)
+
+	def push_event(self, event):
+		try:
+			rpipe = self.redis.pipeline()
+			rpipe.rpush(self.queue,str(event))
+			rpipe.decr(CONSUMED_EVENTS,1) # Decrement in redis the number of NCDRs consumed
+			rpipe.execute()
+			self.current_event = None
+		except redis.ConnectionError:
+			pass
