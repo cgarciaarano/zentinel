e9a0307f5c3255ce1f8e96b41e1a65ee2d082f0b
cgarciaarano <cgarciaarano@gmail.com>
2014-01-17 19:11:25 +0100

    Now compiles, but do nothing

--- core/event_queue.py
+++ core/event_queue.py
@@ -11,7 +11,14 @@ Event Queue
 @author Carlos Garcia <cgarciaarano@gmail.com>
 """
 from event import Event
+import logging
+import sys
+import time
 
+logger = logging.getLogger('kk')
+sys.path.insert(0, '../')
+
+import web.settings
 class EventQueue():
 	"""
 	Queue of events. It uses Redis as backend, and permits duplicated events.
@@ -21,7 +28,7 @@ class EventQueue():
 		self.queue = 'EVENT_QUEUE'
 		self.redis = self.redis_connect()
 
-	def redis_connect():
+	def redis_connect(self):
 		''' Reconnects to Redis and returns an active connection. Exits the program if it can't '''
 		attempts = 1
 		got_redis = False
@@ -29,17 +36,17 @@ class EventQueue():
 		while not got_redis and attempts < 10:
 			try:
 				logger.info("Trying Redis reconnection {0}...".format(attempts))
-				rds = redis.Redis(options.redisIP,db=options.redisDB)
+				rds = redis.Redis(web.settings.REDIS_IP,db=web.settings.REDIS_DB)
 				rds.llen(self.queue)
 				got_redis = True
 				logger.info("Redis connected!")
 			except Exception, e:
-				logger.error("Could not connect to Redis at {0}: {1}. Trying again in 1 second".format(options.redisIP,e))
+				logger.error("Could not connect to Redis at {0}: {1}. Trying again in 1 second".format(web.settings.REDIS_IP,e))
 				time.sleep(1)
 				attempts += 1
 
 		if attempts == 10:
-			logger.error("Reconnection to Redis {0} failed 10 times, exiting program".format(options.redisIP))
+			logger.error("Reconnection to Redis {0} failed 10 times, exiting program".format(web.settings.REDIS_IP))
 			sys.exit(-1)
 
 		return rds
