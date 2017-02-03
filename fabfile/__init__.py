f7740ef8070a9321e1b84ebedf313588c8ca6c53
vagrant <vagrant@precise64.(none)>
2017-02-03 13:20:22 +0000

    Core files without secrets

---
+++ fabfile/__init__.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from fabric.api import *
+from fabric.contrib.console import confirm
+from cuisine import *
+import os
+import sys
+from fabric.colors import red, green
+import circus, redis, asterisk, mysql, postgresql
+
+env.user = 'vagrant'
+env.password = 'vagrant'
+
+PROJECT_ROOT = '/vagrant/'
+ENV_ROOT = '/home/vagrant/'
+
+users = {	'your-user':'user',
+		}
+
+# System stuff
+def addUsers():
+	for user,passwd in users.iteritems():
+		user_ensure(user,passwd)
+		group_user_ensure('sudo',user)
+
+def custom_asterisk():
+	with mode_sudo():
+		configure_tts()
+
+		file_link(PROJECT_ROOT + 'fabfile/asterisk/zentinel.ael','/etc/asterisk/zentinel.ael',owner='asterisk')
+		file_append('/etc/asterisk/extensions.ael','#include "zentinel.ael"')
+
+def custom_circus():
+	with mode_sudo():
+		file_link(PROJECT_ROOT + 'fabfile/circus/circus.ini','/etc/circus/circus.ini',owner='root')
+		dir_ensure('/var/log/zentinel')
+
+def custom_mysql():
+	with mode_sudo():
+		# Create users
+		# grant all privileges on zentinel.* to zentinel@'%' identified by 'flask';
+		pass
+
+def custom_postgresql():
+	sudo('psql -c "{0}"'.format('create database zentinel;'), user='postgres')
+	sudo('psql -c "{0}"'.format('alter user postgres with password \'PASSWORD\';'), user='postgres')
+	pass
+	
+def logs():
+	with mode_sudo():
+		puts(green('Configuring zentinel logs...'))
+		config_template = text_strip_margin('''
+		|local0.*        /var/log/zentinel/web.log
+		|local1.*        /var/log/zentinel/core.log
+		''')
+		file_write('/etc/rsyslog.d/zentinel.conf', config_template)
+
+def logrotate():
+	with mode_sudo():
+		exist = file_exists("/etc/logrotate.d/zentinel")
+	  	if not exist:
+			run('touch /etc/logrotate.d/circus')
+		config_template = text_strip_margin('''
+		|/var/log/zentinel/*.log
+		|{
+		|        rotate 7
+		|        daily
+		|        missingok
+		|        size 500M
+		|        notifempty
+		|        copytruncate
+		|        delaycompress
+		|        compress
+		|}
+		''')
+		file_write('/etc/logrotate.d/zentinel', config_template)
+
+def configure_tts():
+	package_ensure('sox')
+	package_ensure('mpg123')
+	package_ensure('perl')
+	package_ensure('libwww-perl')
+
+	run('wget https://github.com/zaf/asterisk-googletts/archive/v0.6.tar.gz')
+	run('tar xzvf v0.6.tar.gz')
+	run('mv asterisk-googletts-0.6/googletts.agi /usr/share/asterisk/agi-bin')
+	dir_remove('v0.6.tar.gz')
+
+
+def configure_web():
+	python_package_ensure_pip('chaussette')
+	package_ensure('libev-dev')
+	python_package_ensure_pip('bjoern')
+
+@task
+def setup_system():
+	addUsers()
+
+@task
+def configure_system():
+	with mode_sudo():
+		package_update()
+		package_ensure('vim')
+		package_ensure('curl')
+
+		# Software (role dependant)
+		postgresql.deploy()
+		redis.deploy()
+		asterisk.deploy()
+		circus.deploy()
+		
+		custom_asterisk()
+		custom_circus()
+		custom_postgresql()
+
+		# System stuff
+		configure_web()
+		logs()
+		logrotate()
+		
+# Application stuff
+@task
+def app_environment():
+	# BROKEN
+	# Don't work in Vagrant Windows 
+	# with cd(PROJECT_ROOT):
+		if not dir_exists('{0}/env/'.format(ENV_ROOT)):
+			sudo('wget https://raw.github.com/pypa/virtualenv/1.9.X/virtualenv.py')
+			sudo('python virtualenv.py {0}env/'.format(ENV_ROOT))
+			sudo('env/bin/pip install -r {0}requirements.txt'.format(PROJECT_ROOT))
+
+def python_requisites():
+	with mode_sudo():
+		# This should be in requisites.txt
+		python_package_ensure_pip('redis')
+		python_package_ensure_pip('hiredis')
+		python_package_ensure_pip('rq')
+		python_package_ensure_pip('Flask')
+		python_package_ensure_pip('Flask-Cache')
+		python_package_ensure_pip('Flask-SQLAlchemy')
+		python_package_ensure_pip('flask-migrate')
+		python_package_ensure_pip('Flask-WTF')
+		python_package_ensure_pip('Flask-Login')
+		python_package_ensure_pip('Flask-OpenID')
+		python_package_ensure_pip('psycopg2')
+
+def prerequisites():
+	package_ensure('python-dev')
+	package_ensure('libevent-dev')
+	package_ensure('libpq-dev')
+	python_requisites()
+
+@task
+def deploy():
+	#setup_system()
+	configure_system()
+	prerequisites()
+	#app_environment()
+
---
+++ zentinel/settings.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+from logging.handlers import SysLogHandler 
+import logging.config
+
+import os
+import redis
+import hiredis
+
+DEBUG = True
+
+SYSLOG_FACILITY = 'local0'
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
+TIME_ZONE = 'Europe/Paris'
+
+USE_TZ = True
+
+APP_NAME = 'zentinel'
+
+#########################################################################################
+###########                       WEB CONFIG                              #############
+#########################################################################################
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+WEB_IPADDR = 'localhost'
+WEB_PORT = 8080
+
+DBDRIVER = 'postgresql'
+DBUSER = 'postgres'
+DBPASSWD = 'PASSWORD'
+DBSERVER = 'localhost'
+DBPORT = '5432'
+DBNAME = 'zentinel'
+
+SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}:{4}/{5}'.format(DBDRIVER,DBUSER,DBPASSWD,DBSERVER,DBPORT,DBNAME)
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_migrations')
+
+SQLALCHEMY_BINDS = {
+	'db_web' : SQLALCHEMY_DATABASE_URI,
+	'db_data' : SQLALCHEMY_DATABASE_URI,
+	}
+
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+#########################################################################################
+###########                       CORE CONFIG                              #############
+#########################################################################################
+API_IPADDR = 'localhost'
+API_PORT = 8000
+
+#########################################################################################
+###########                       REDIS CONFIG                              #############
+#########################################################################################
+REDIS_IP = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 1
+EXPIRATION = 60
+
+REDIS_EQUEUE_IP = REDIS_IP
+REDIS_EQUEUE_DB = REDIS_DB
+REDIS_EQUEUE_PORT = REDIS_PORT
+# REDIS_POOL = redis.ConnectionPool(max_connections=500, unix_socket_path='/var/run/redis/redis.sock')
+REDIS_EVENT_POOL = redis.ConnectionPool(max_connections=500, host=REDIS_EQUEUE_IP, db=REDIS_EQUEUE_DB, port=REDIS_EQUEUE_PORT)
+
+REDIS_WQUEUE_IP = REDIS_IP
+REDIS_WQUEUE_DB = REDIS_DB
+REDIS_WQUEUE_PORT = REDIS_PORT
+REDIS_WORKER_POOL = redis.ConnectionPool(max_connections=500, host=REDIS_WQUEUE_IP, db=REDIS_WQUEUE_DB, port=REDIS_WQUEUE_PORT)
+
+CONSUMED_EVENTS = 'CONSUMED_EVENTS'
+
+#########################################################################################
+###########                       CACHE CONFIG                              #############
+#########################################################################################
+CACHES = {
+	'default': {
+		'CACHE_TYPE': 'redis',
+		'CACHE_KEY_PREFIX': APP_NAME,
+		'CACHE_DEFAULT_TIMEOUT':2,
+		'CACHE_REDIS_HOST':REDIS_IP,
+		'CACHE_REDIS_PORT': REDIS_PORT,
+		'CACHE_REDIS_DB':REDIS_DB,
+		'OPTIONS': {
+			'PARSER_CLASS':'redis.connection.HiredisParser',
+		},
+	},
+}
+#########################################################################################
+###########                       RQ CONFIG                                 #############
+#########################################################################################
+RQ_TIMEOUT = 1800
+"""
+RQ_QUEUES = {
+	'default': {
+		'HOST': REDIS_IP,
+		'PORT': 6379,
+		'DB': REDIS_DB,
+		'PASSWORD': '',
+	},
+}
+"""
+#########################################################################################
+###########                       ASTERISK CONFIG                           #############
+#########################################################################################
+ASTERISK_AMI_SERVERS = [ {	'host':'localhost',
+							'port':8088,
+							'user':'zentinel',
+							'passwd':'PASSWORD'} ]
+
+#########################################################################################
+###########                       LOGGING CONFIG                            #############
+#########################################################################################
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': True,
+	'formatters': {
+		'verbose': {
+			'format': '%(asctime)s PID:%(process)s %(filename)s %(funcName)s %(levelname)s %(message)s'
+		},
+		'simple': {
+			'format': '%(levelname)s %(message)s'
+		},
+	},
+	'filters': {},
+	'handlers': {
+		'console':{
+			'level':'DEBUG',
+			'class':'logging.StreamHandler',
+			'formatter': 'verbose'
+		},
+		 'rsyslog0': {
+			'level': 'DEBUG',
+			'class': 'logging.handlers.SysLogHandler',
+			'formatter': 'verbose',
+			'facility': 'local0',
+			'address': '/dev/log',
+			'filters': []
+		},
+		'rsyslog1': {
+			'level': 'DEBUG',
+			'class': 'logging.handlers.SysLogHandler',
+			'formatter': 'verbose',
+			'facility': 'local1',
+			'address': '/dev/log',
+			'filters': []
+		},
+	},
+	'loggers': {
+		"web": {
+			"handlers": ['console',"rsyslog0"],
+			"level": "DEBUG",
+			"progagate": True,
+		},
+		"core": {
+			"handlers": ['console',"rsyslog1"],
+			"level": "DEBUG",
+			"progagate": True,
+		},
+	}
+}
+logging.config.dictConfig(LOGGING)
